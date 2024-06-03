813c9b27b1f186286e46b074230d735c3873acb0
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-06-02 22:45:41 -0400

    Made additions to the project

--- views.py
+++ views.py
@@ -5,4 +5,6 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name="Jordan", age=23, quote="Jack of all "
+                                                                            "trades, master of none, but still better"
+                                                                            " than a master of one. - Shakespeare")
